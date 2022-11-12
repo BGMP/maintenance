9a456222f8f103b64468bb662a40ba04ee460e2a
bamm99 <benjamin.mosso1701@alumnos.ubiobio.cl>
2022-11-12 15:14:41 -0300

    api: add details, update and delete user request
    Se agregaron las funciones userDetails, update y delete al userController y su respectivas rutas en userRoutes.js.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,8 @@ const usersController = require('../controllers/usersController')
 
 api.post('/new_user', usersController.createUser)
 api.get('/users', usersController.getUsers)
+api.delete('/del_company/:id', usersController.userDelete)
+api.get('/users/user_id/:id', usersController.userDetails)
+api.patch('/user_update/:id', usersController.userUpdate)
 
 module.exports = api
