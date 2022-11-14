70737795f9c5d5c466ef7fb3c690217f0c9d32ab
bamm99 <benjamin.mosso1701@alumnos.ubiobio.cl>
2022-11-14 01:27:28 -0300

    api: integration of validations
    se integró validaciones a company.js

--- models/company.js
+++ models/company.js
@@ -4,31 +4,45 @@ const CompanySchema = new Schema({
     name: {
         type: String,
         required: true,
+        minlength: 5,
+        maxlength: 30
     },
     email: {
         type: String,
-        required: true
-    },
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
     rut: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minlength: 11,//1.234.567-8
+        maxlength: 12//12.345.678-9
     },
     area: {
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        maxlength: 30
     },
     phone: {
         type: String,
-        required: true
+        required: true,
+        minlength: 9,
+        match: [/^[9][0-9]{1,8}$/]
     },
     address: {
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        maxlength: 60
     },
     contact: {
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        maxlength: 40
     }
 })
 
