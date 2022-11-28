cec5e0b9a7489c28422557669eef9dc9071b8334
pipe-r-v <felipe.rodriguez1701@alumnos.ubiobio.cl>
2022-11-28 03:37:52 -0300

    api: add auth middleware for /companies

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const api = express.Router()
 const companiesController = require('../controllers/companiesController')
+const {authGetCompanies} = require('../middlewares/companyMiddlewares')
+const {authPostCompanies} = require('../middlewares/companyMiddlewares')
+const {authDeletCompanies} = require('../middlewares/companyMiddlewares')
+const {authUpdateCompanies} = require('../middlewares/companyMiddlewares')
+const {authGetsCompanies} = require('../middlewares/companyMiddlewares')
 
-api.post('/company', companiesController.createCompany)
-api.get('/company/:id', companiesController.getCompany)
-api.delete("/company/:id", companiesController.deleteCompany)
-api.patch("/company/:id", companiesController.updateCompany)
-api.get('/companies', companiesController.getCompanies)
+
+api.post('/company', authPostCompanies(["admin"]), companiesController.createCompany)
+api.get('/company/:id', authGetCompanies(["admin"]), companiesController.getCompany)
+api.delete("/company/:id", authDeletCompanies(["admin"]), companiesController.deleteCompany)
+api.patch("/company/:id", authUpdateCompanies(["admin"]), companiesController.updateCompany)
+api.get('/companies', authGetsCompanies(["admin"]), companiesController.getCompanies)
 
 module.exports = api
