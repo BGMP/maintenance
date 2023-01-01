de0c5e1ee6a11fb7b0be8789c96a90d06d2e0cb5
José Benavente <jose@bgm.cl>
2023-01-01 15:05:18 -0300

    refactor: rename company delete middleware
    - authDeletCompanies => authDeleteCompanies

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -3,14 +3,14 @@ const api = express.Router()
 const companiesController = require('../controllers/companiesController')
 const {authGetCompanies} = require('../middlewares/companyMiddlewares')
 const {authPostCompanies} = require('../middlewares/companyMiddlewares')
-const {authDeletCompanies} = require('../middlewares/companyMiddlewares')
+const {authDeleteCompanies} = require('../middlewares/companyMiddlewares')
 const {authUpdateCompanies} = require('../middlewares/companyMiddlewares')
 const {authGetsCompanies} = require('../middlewares/companyMiddlewares')
 
 
 api.post('/company', authPostCompanies(["admin"]), companiesController.createCompany)
 api.get('/company/:id', authGetCompanies(["admin"]), companiesController.getCompany)
-api.delete("/company/:id", authDeletCompanies(["admin"]), companiesController.deleteCompany)
+api.delete("/company/:id", authDeleteCompanies(["admin"]), companiesController.deleteCompany)
 api.patch("/company/:id", authUpdateCompanies(["admin"]), companiesController.updateCompany)
 api.get('/companies', authGetsCompanies(["admin"]), companiesController.getCompanies)
 
