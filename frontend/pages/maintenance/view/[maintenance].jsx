ff61bda75eb3bda13d31185aa601cdc063fdf0e6
José Benavente <jose@bgm.cl>
2023-01-02 06:40:00 -0300

    frontend: add components and more axios requests

---
+++ frontend/pages/maintenance/view/[maintenance].jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import { getProduct } from '../../../data/maintenances'
+import { Button, Container, Heading, HStack, Stack, Text } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import ShowInfo from '../../../components/ShowInfo'
+
+export const getServerSideProps = async (context) => {
+    try {
+        const response = await getProduct(context.query.maintenance, localStorage.getItem('token'))
+        if (response.status === 200) {
+            return {
+                props: {
+                    data: response.data
+                }
+            }
+        } else {
+            return {
+                redirect: {
+                    destination: "/",
+                }
+            }
+        }
+    } catch (error) {
+        return {
+            redirect: {
+                destination: "/",
+            }
+        }
+    }
+
+}
+
+const editar = ({ data }) => {
+    const [maintenance] = useState(data)
+    const router = useRouter()
+
+    return (
+        <Container maxW="container.xl" mt={10}>
+            <Heading as={"h1"} size={"2xl"} textAlign={"center"}>Producto: {product.name}</Heading>
+            <Stack my={10}>
+                <ShowInfo value={maintenance.name} color={"green.300"} tag={"Nombre"} />
+                <ShowInfo value={maintenance.description} color={"blue.300"} tag={"Descripción"} />
+            </Stack>
+            <HStack >
+                <Button w={"full"} colorScheme="blue" mt={10} mb={10}>Editar</Button>
+                <Button w={"full"} colorScheme="red" mt={10} mb={10}>Eliminar</Button>
+                <Button w={"full"} colorScheme="green" mt={10} mb={10} onClick={() => router.push("/")}>Volver</Button>
+            </HStack>
+        </Container>
+    )
+}
+
+export default editar
