5d9331fda9c0d8af9f0459fd386df8da8784d6fb
José Benavente <jose@bgm.cl>
2023-01-03 04:33:50 -0300

    frontend: implement basic navbar

--- frontend/data/maintenances.js
+++ frontend/data/maintenances.js
@@ -1,35 +1,31 @@
 import axios from 'axios';
 
 const getMaintenances = async () => {
-    return await axios.get("http://localhost:3010/api/maintenances")
+    return await axios.get(`${process.env.API}/maintenances`)
+}
+
+const getMaintenance = async (id) => {
+    return await axios.get(`${process.env.API}/maintenance/${id}`)
 }
 
 const createMaintenance = (maintenance) => {
-    const response = axios.post(`${process.env.SERVIDOR}/maintenances`, {
+    return axios.post(`${process.env.API}/maintenances`, {
         company: maintenance.company,
         target: maintenance.target,
         type: maintenance.type,
         description: maintenance.description,
         start_date: maintenance.start_date,
         end_date: maintenance.end_date
-    });
-    return response
-}
-
-const getMaintenance = async (id) => {
-    console.log(id)
-    const response = await axios.get(`${process.env.SERVIDOR}/maintenance/${id}`)
-    return response
+    })
 }
 
 const updateMaintenance = (id, maintenance) => {
-    const response = axios.put(`${process.env.SERVIDOR}/maintenance/${id}`, maintenance)
-    return response
+    return axios.patch(`${process.env.API}/maintenance/${id}`, maintenance)
 }
 
 module.exports = {
     getMaintenances,
-    createMaintenance,
     getMaintenance,
-    updateMaintenance
+    updateMaintenance,
+    createMaintenance,
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import { ChakraProvider } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
       <ChakraProvider>
-        <Component {...pageProps} />
+          <NavBar>
+              <Component {...pageProps} />
+          </NavBar>
       </ChakraProvider>
   )
 }
